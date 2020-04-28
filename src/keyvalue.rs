82a77e0598a5e27d46ade236b9335df175e98f69
kevingzhang <kevin.zhang.canada@gmail.com>
2020-04-28 13:55:44 -0700

    use my own tea-codec. no new features added yet

---
+++ src/keyvalue.rs
@@ -0,0 +1,192 @@
+//! # Key-Value Store Data Types
+//!
+//! This module contains data types for the `wascc:keyvalue` capability provider
+
+pub const OP_ADD: &str = "Add";
+pub const OP_GET: &str = "Get";
+pub const OP_SET: &str = "Set";
+pub const OP_DEL: &str = "Del";
+pub const OP_CLEAR: &str = "Clear";
+pub const OP_RANGE: &str = "Range";
+pub const OP_PUSH: &str = "Push";
+pub const OP_LIST_DEL: &str = "ListItemDelete";
+
+pub const OP_SET_ADD: &str = "SetAdd";
+pub const OP_SET_REMOVE: &str = "SetRemove";
+pub const OP_SET_UNION: &str = "SetUnion";
+pub const OP_SET_INTERSECT: &str = "SetIntersection";
+pub const OP_SET_QUERY: &str = "SetQuery";
+pub const OP_KEY_EXISTS: &str = "KeyExists";
+
+/// A request to get a single value from the K/V store
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetRequest {
+    pub key: String,
+}
+
+/// The result of a get request
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetResponse {
+    /// The value returned from the data store
+    pub value: String,
+    /// Indicates whether the key existed
+    pub exists: bool,
+}
+
+/// A request to set a value
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetRequest {
+    /// Key of the item to set
+    pub key: String,
+    /// Value of the item to set
+    pub value: String,
+    /// Seconds after which the key will expire, 0 - no expiration
+    #[serde(rename = "expires")]
+    pub expires_s: i32,
+}
+
+/// A request to delete a key
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DelRequest {
+    pub key: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DelResponse {
+    pub key: String,
+}
+
+/// Response to a set request
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetResponse {
+    pub value: String,
+}
+
+/// A request to perform an atomic add operation
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AddRequest {
+    pub key: String,
+    pub value: i32,
+}
+
+/// Result of an atomic add operation
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AddResponse {
+    pub value: i32,
+}
+
+/// A request to add an item to the end of a list
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ListPushRequest {
+    pub key: String,
+    pub value: String,
+}
+
+/// A request to delete all occurences of an item from a list
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ListDelItemRequest {
+    pub key: String,
+    pub value: String,
+}
+
+/// A request to clear a list at a given key
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ListClearRequest {
+    pub key: String,
+}
+
+/// A request to retrieve a range of values from a list
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ListRangeRequest {
+    pub key: String,
+    pub start: i32,
+    pub stop: i32,
+}
+
+/// List of values returned from a range request
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ListRangeResponse {
+    #[serde(default)]
+    pub values: Vec<String>,
+}
+
+/// Return response from non-range list requests like push and clear
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ListResponse {
+    pub new_count: i32,
+}
+
+/// Request to add an item to a set
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetAddRequest {
+    pub key: String,
+    pub value: String,
+}
+
+/// Request to remove a specific value from a set
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetRemoveRequest {
+    pub key: String,
+    pub value: String,
+}
+
+/// Request to query the contents of a set
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetQueryRequest {
+    pub key: String,
+}
+
+/// Response to an operation that requests members of a list (query, intersect, union)
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetQueryResponse {
+    #[serde(default)]
+    pub values: Vec<String>,
+}
+
+/// Request for the intersection of multiple sets
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetIntersectionRequest {
+    #[serde(default)]
+    pub keys: Vec<String>,
+}
+
+/// Request for the union of multiple sets
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetUnionRequest {
+    #[serde(default)]
+    pub keys: Vec<String>,
+}
+
+/// Response to a set query, add, or delete
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetOperationResponse {
+    pub new_count: i32,
+}
+
+/// Test for the existence of a key
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct KeyExistsQuery {
+    pub key: String,
+}
---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+#![doc(html_logo_url = "https://avatars2.githubusercontent.com/u/52050279?s=200&v=4")]
+// Copyright 2015-2020 Capital One Services, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! # wascc-codec
+//!
+//! This library provides the core set of types and associated functions used to facilitate actor
+//! and host runtime communication for waSCC.
+
+/// The version of the codec as seen on crates.io
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[macro_use]
+extern crate serde_derive;
+extern crate log;
+
+extern crate rmp_serde as rmps;
+use rmps::{Deserializer, Serializer};
+use serde::{Deserialize, Serialize};
+use std::io::Cursor;
+
+/// The standard function for serializing codec structs into a format that can be
+/// used for message exchange between actor and host. Use of any other function to
+/// serialize could result in breaking incompatibilities.
+pub fn serialize<T>(item: T) -> ::std::result::Result<Vec<u8>, Box<dyn ::std::error::Error>>
+where
+    T: Serialize,
+{
+    let mut buf = Vec::new();
+    item.serialize(&mut Serializer::new(&mut buf).with_struct_map())?;
+    Ok(buf)
+}
+
+/// The standard function for de-serializing codec structs from a format suitable
+/// for message exchange between actor and host. Use of any other function to
+/// deserialize could result in breaking incompatibilities.
+pub fn deserialize<'de, T: Deserialize<'de>>(
+    buf: &[u8],
+) -> ::std::result::Result<T, Box<dyn ::std::error::Error>> {
+    let mut de = Deserializer::new(Cursor::new(buf));
+    match Deserialize::deserialize(&mut de) {
+        Ok(t) => Ok(t),
+        Err(e) => Err(format!("Failed to de-serialize: {}", e).into()),
+    }
+}
+
+pub mod keyvalue;
