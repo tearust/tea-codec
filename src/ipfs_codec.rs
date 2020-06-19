60bdc66e1b3ebad98a93666e558da394265f48c8
kevingzhang <kevin.zhang.canada@gmail.com>
2020-06-18 18:56:56 -0700

    update ipfs

--- src/ipfs_codec.rs
+++ src/ipfs_codec.rs
@@ -1,4 +1,9 @@
 pub const OP_DELIVER_SUB_MESSAGE: &'static str = "deliver_sub_message";
+pub const OP_PUBSUB_SUB: &'static str = "pubsub_sub";
+pub const OP_BLOCK_GET: &'static str = "block_get";
+pub const OP_BLOCK_PUT: &'static str = "block_put";
+
+
 /// Describes an HTTP request
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -8,4 +13,25 @@ pub struct PubsubSubResponse {
   pub seqno: Option<Vec<u8>>,
   pub topics: Option<Vec<String>>, 
   pub unrecognized: Option<Vec<u8>>,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PubsubSubRequest{
+  pub topic: String,
+  pub discover: bool,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PubsubPubRequest{
+  pub topic: String,
+  pub payload: Vec<u8>,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BlockPutResponse{
+  pub key: String,
+  pub size: u64,
 }
