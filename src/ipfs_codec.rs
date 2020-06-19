c730af2098e8a7baf5551602fd9a4e7047b0bf96
kevingzhang <kevin.zhang.canada@gmail.com>
2020-06-19 14:46:50 -0700

    add random topic for task sub

--- src/ipfs_codec.rs
+++ src/ipfs_codec.rs
@@ -1,16 +1,33 @@
 pub const OP_DELIVER_SUB_MESSAGE: &'static str = "deliver_sub_message";
 pub const OP_PUBSUB_SUB: &'static str = "pubsub_sub";
+pub const OP_PUBSUB_RANDOM_SUB: &'static str = "pubsub_random_sub";
+
+pub const OP_PUBSUB_KILLSUB: &'static str = "pubsub_killsub";
 pub const OP_BLOCK_GET: &'static str = "block_get";
 pub const OP_BLOCK_PUT: &'static str = "block_put";
+
+
+pub const RSA_PUBKEY: &'static str = "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0NCk1Ed3dEUVlKS29aSWh2Y05\
+BUUVCQlFBREt3QXdLQUloQU1KYUlLVkZHbWZMMzNNa3Rvb2ZxclBKUXBic09WT3UNCkQ5Q0lFRnEvcURMRkFnTUJBQUU9\
+DQotLS0tLUVORCBQVUJMSUMgS0VZLS0tLS0NCg==";
+
+pub const RSA_PRIKEY: &'static str = "LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQ0KTUlHckFnRUFBaUV\
+Bd2xvZ3BVVWFaOHZmY3lTMmloK3FzOGxDbHV3NVU2NFAwSWdRV3Irb01zVUNBd0VBQVFJZw0KS3FjN0NPcHNqQ0ViTGhy\
+OWtMeTRhcEJ6WTJUVE5NdlFyZUpwa2VTbUYwRUNFUUR2NDB3NmdyUEpyc0VqSFM5Vw0KYkNhVkFoRUF6MmZmb1hQUWpib\
+m1OTkJaaGhxL2NRSVJBTEwwUTVlYWpYaTFMMHQ4cUNXMmhJa0NFUUNMc1kzSg0KSThWTFAvT1NqT1pQVWNLeEFoQk1oal\
+hWTWNXMnh6NGFqK25sU05CbQ0KLS0tLS1FTkQgUlNBIFBSSVZBVEUgS0VZLS0tLS0NCg==";
+
+pub const ED_PUBKEY: &'static str = "df38cb4f12479041c8e8d238109ef2a150b017f382206e24fee932e637c2db7b";
+pub const ED_PRIKEY: &'static str = "5579a3c220146f0caaab49b884de505098b89326970b929d781cf4a65445a917df38cb4f12479041c8e8d238109ef2a150b017f382206e24fee932e637c2db7b";
 
 
 /// Describes an HTTP request
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PubsubSubResponse {
-  pub from: Option<Vec<u8>>,
-  pub data: Option<Vec<u8>>,
-  pub seqno: Option<Vec<u8>>,
+  pub from: Option<String>,
+  pub data: Option<String>,
+  pub seqno: Option<String>,
   pub topics: Option<Vec<String>>, 
   pub unrecognized: Option<Vec<u8>>,
 }
@@ -24,9 +41,21 @@ pub struct PubsubSubRequest{
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
+pub struct PubsubRandomSubRequest{
+  pub random: u32,
+  pub discover: bool,
+}
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PubsubRandomSubResponse{
+  pub topic: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct PubsubPubRequest{
   pub topic: String,
-  pub payload: Vec<u8>,
+  pub payload: String,
 }
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
@@ -34,4 +63,17 @@ pub struct PubsubPubRequest{
 pub struct BlockPutResponse{
   pub key: String,
   pub size: u64,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PubsubKillSubRequest{
+  pub topic: String,
+  pub curse: String,
+}
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PubsubKillSubResponse{
+  pub curse: String,
+  pub err: Option<String>,
 }
