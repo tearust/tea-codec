65c037febb306cfa7763a8a2b353acbf931e59b0
jacky.li <jacky.li@accenture.com>
2020-06-29 02:34:05 +0800

    udpate task mod

--- src/task.rs
+++ src/task.rs
@@ -6,3 +6,9 @@ pub const START: &'static str = "start_task_from_runtime";
 pub struct TensorflowParam {
   pub image: Vec<u8>,
 }
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TensorflowResult {
+  pub result: String,
+}
