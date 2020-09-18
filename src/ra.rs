572980d15667f7601cabd36e367c8249de615293
jacky.li <jacky.li@accenture.com>
2020-09-18 15:26:10 +0800

    update ra init payload

--- src/ra.rs
+++ src/ra.rs
@@ -4,6 +4,6 @@ pub const OP_INIT: &'static str = "opt_ra_init";
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct InitPayload {
-  pub cid_manifest: String,
+  pub manifest_cid: String,
   pub manifest_str: String,
 }
