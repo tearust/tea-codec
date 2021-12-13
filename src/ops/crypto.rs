47bc128d602654ed03e4aed4ef1087e25def0ef7
Yan Mingzhi <realraindust@gmail.com>
2021-12-13 19:57:57 +0800

    add operation about cryto

---
+++ src/ops/crypto.rs
@@ -0,0 +1,17 @@
+pub const OP_GENERATE_KEY_PAIR: &str = "GenerateKeyPair";
+pub const OP_SIGN: &str = "Sign";
+pub const OP_VERIFY: &str = "Verify";
+pub const OP_SHA256: &str = "Sha";
+pub const OP_SHAMIR_SHARE: &str = "ShamirShare";
+pub const OP_SHAMIR_RECOVERY: &str = "ShamirRecovery";
+pub const OP_GENERATE_MULTI_SIG_ASSET: &str = "GenerateMultiSigAsset";
+pub const OP_COMBINE_TO_WITNESS: &str = "CombineToWitness";
+pub const OP_GENERATE_AES_KEY: &str = "GenerateAesKey";
+pub const OP_AES_ENCRYPT: &str = "AesEncrypt";
+pub const OP_AES_DECRYPT: &str = "AesDecrypt";
+pub const OP_GENERATE_RSA_PKCS1: &str = "GenerateRsaPkcs1";
+pub const OP_RSA_DECRYPT: &str = "RsaDecrypt";
+pub const OP_RSA_ENCRYPT: &str = "RsaEncrypt";
+pub const OP_FROM_SS58_ADDRESS: &str = "FromSS58";
+pub const OP_TO_SS58_ADDRESS: &str = "ToSS58";
+pub const OP_PUB_FROM_PRI_KEY: &str = "PubFromPrivateKey";
