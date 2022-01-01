10a7f5bdd16592141df6d670244969aae9aad0a6
kevingzhang <kevin.zhang.canada@gmail.com>
2022-01-01 21:10:17 +0000

    add new message

--- src/ops/token_state.rs
+++ src/ops/token_state.rs
@@ -20,3 +20,5 @@ pub const OP_PAYMENT_FROM_DEPOSIT: &str = "PaymentFromDeposit";
 pub const OP_EXPORT_STATE: &str = "ExportState";
 pub const OP_IMPORT_STATE: &str = "ImportState";
 pub const OP_SET_AUTH_OPS_BYTES: &str = "SetAuthOpsBytes";
+pub const OP_SET_USER_LOGIN_SESSION_KEY: &str = "SetUserLoginSessionKey";
+pub const OP_GET_USER_LOGIN_SESSION_KEY: &str = "GetUserLoginSessionKey";
