5947401651b3963b9fedc74743c80fddfcd14d2e
kevingzhang <kevin.zhang.canada@gmail.com>
2022-01-25 00:29:20 +0000

    clean code

--- src/ops/token_state.rs
+++ src/ops/token_state.rs
@@ -25,5 +25,4 @@ pub const OP_INIT_GLUESQL: &str = "InitGlueSql";
 pub const OP_EXEC_GLUECMD: &str = "ExecGlueCmd";
 pub const OP_EXEC_GLUEQUERY: &str = "ExecGlueQuery";
 pub const OP_SET_AUTH_OPS_BYTES: &str = "SetAuthOpsBytes";
-// pub const OP_SET_USER_LOGIN_SESSION_KEY: &str = "SetUserLoginSessionKey";
 pub const OP_QUERY_USER_LOGIN_SESSION_KEY: &str = "QueryUserLoginSessionKeyRequest";
