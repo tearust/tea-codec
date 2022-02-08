505bd5d47c99021f1cabf2a01d6a95568a3510d5
Yan Mingzhi <realraindust@gmail.com>
2022-02-08 21:08:14 +0800

    add pprof profile related

--- src/ops/env.rs
+++ src/ops/env.rs
@@ -4,3 +4,4 @@ pub const OP_CURRENT_TIMESTAMP: &str = "CurrentTimestamp";
 pub const OP_TASK_START: &str = "TaskStart";
 pub const OP_TASK_END: &str = "TaskEnd";
 pub const OP_REGISTER_RANDOM_TICK: &str = "RegisterRandomTick";
+pub const OP_PPROF_PROFILE: &str = "PprofProfile";
