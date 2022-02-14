bc125ac73aab9801be1e7d2df3de0c79e3e6f779
Yan Mingzhi <realraindust@gmail.com>
2022-02-14 16:29:31 +0800

    add get exec cursor related

--- src/ops/replica.rs
+++ src/ops/replica.rs
@@ -37,3 +37,4 @@ pub const OP_FIND_EXECUTED_TXN: &str = "FindExecutedTxn";
 pub const OP_GET_CONSENSUS_REPLICAS: &str = "GetConsensusReplicas";
 pub const OP_SET_SINGLE_MODE: &str = "SetSingleMode";
 pub const OP_REPORT_TXN_EXEC_ERR: &str = "ReportTxnExecError";
+pub const OP_GET_EXEC_CURSOR: &str = "GetExecCursor";
