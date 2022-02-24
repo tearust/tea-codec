2b1cdf4af120df2da7738deb2b1f909a954614a3
kevingzhang <kevin.zhang.canada@gmail.com>
2022-02-24 18:53:21 +0000

    commit with many todos, adding consume to divident action

--- src/ops/token_state.rs
+++ src/ops/token_state.rs
@@ -19,6 +19,7 @@ pub const OP_REFUND: &str = "Refund";
 pub const OP_CONSUME_FROM_DEPOSIT: &str = "ConsumeFromDeposit";
 pub const OP_CONSUME_FROM_ACCOUNT: &str = "ConsumeFromAccount";
 pub const OP_PAYMENT_FROM_DEPOSIT: &str = "PaymentFromDeposit";
+pub const OP_BURN_CONSUME_UNBALANCED: &str = "BurnConsumeUnbalance";
 pub const OP_EXPORT_STATE: &str = "ExportState";
 pub const OP_IMPORT_STATE: &str = "ImportState";
 pub const OP_EXPORT_GLUESQL: &str = "ExportGlueSql";
