4530f1b8db33782110800fd86f39380a6b7ece17
Yan Mingzhi <realraindust@gmail.com>
2022-05-06 17:49:08 +0800

    update callback defines

--- src/ops.rs
+++ src/ops.rs
@@ -8,13 +8,8 @@ pub mod state_receiver;
 pub mod token_state;
 
 pub const GENERAL_REPLY_PREFIX: &str = "reply";
-pub const LAYER1_ASYNC_REPLY_PREFIX: &str = "layer1_async";
 
 /// The general reply subject
-pub fn general_callback_reply(actor: &str, uuid: &str) -> String {
-	format!("{}.{}.{}", GENERAL_REPLY_PREFIX, actor, uuid)
-}
-
-pub fn layer1_async_reply(actor: &str, uuid: &str) -> String {
-	format!("{}.{}.{}", LAYER1_ASYNC_REPLY_PREFIX, actor, uuid)
+pub fn general_callback_reply(actor: &str, seq_number: u64) -> String {
+	format!("{}.{}.{}", GENERAL_REPLY_PREFIX, actor, seq_number)
 }
