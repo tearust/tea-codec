46c109ea6f57254db2919f5dc95f439e1512c85c
Yan Mingzhi <realraindust@gmail.com>
2022-05-09 14:54:37 +0800

    add general actor interface method

--- src/ops.rs
+++ src/ops.rs
@@ -8,8 +8,13 @@ pub mod state_receiver;
 pub mod token_state;
 
 pub const GENERAL_REPLY_PREFIX: &str = "reply";
+pub const GENERAL_INTERFACE_PREFIX: &str = "actor";
 
 /// The general reply subject
 pub fn general_callback_reply(actor: &str, seq_number: u64) -> String {
 	format!("{}.{}.{}", GENERAL_REPLY_PREFIX, actor, seq_number)
+}
+
+pub fn general_actor_interface(actor: &str, action: &str) -> String {
+	format!("{}.{}.{}", GENERAL_INTERFACE_PREFIX, actor, action)
 }
