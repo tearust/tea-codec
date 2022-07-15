6f2ded25ebdf7fbe16eb5d075d0b80f77a61dded
Yan Mingzhi <realraindust@gmail.com>
2022-07-15 14:59:37 +0800

    add get mining startup related

--- src/ops/replica.rs
+++ src/ops/replica.rs
@@ -40,3 +40,4 @@ pub const OP_GET_EXEC_CURSOR: &str = "GetExecCursor";
 pub const OP_RESET_EXEC_CURSOR: &str = "ResetExecCursor";
 pub const OP_DUMP_TXN_SEQUENCE: &str = "DumpTxnSeq";
 pub const OP_UNSET_SHOULD_LOCK: &str = "UnsetShouldLock";
+pub const OP_GET_MINING_STARTUP: &str = "GetMiningStartup";
