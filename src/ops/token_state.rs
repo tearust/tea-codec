e1684fa86c183faed3bb75b6bce57e3d626340bb
Yan Mingzhi <realraindust@gmail.com>
2022-07-27 14:16:36 +0800

    add get magic number option

--- src/ops/token_state.rs
+++ src/ops/token_state.rs
@@ -43,6 +43,7 @@ pub const OP_DUMP_TAPP_STATES: &str = "DumpTappStates";
 pub const OP_DUMP_GLUEDB_DATA: &str = "DumpGluedbData";
 pub const OP_DUMP_RAW_STATE: &str = "DumpRawState";
 pub const OP_EXTEND_AUTH_KEY: &str = "ExtendAuthKey";
+pub const OP_GET_MAGIC_NUMBER: &str = "GetMagicNumber";
 
 pub const OP_BONDING_BUY: &str = "BondingBuy";
 pub const OP_BONDING_SELL: &str = "BondingSell";
